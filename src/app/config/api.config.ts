e9c02a4ebc6de2a38d9b0ccc00b11603bb68da5e
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-08-21 18:20:06 -0300

    alteração da chave da api

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -2,5 +2,5 @@ export const API_CONFIG = {
     apiBaseUrl: 'https://api.hgbrasil.com/weather?',
     jsonFormatConfig: 'format=json-cors&',
     customResponse: 'array_limit=4&fields=only_results,temp,city,date,forecast,max,min,description,time,weekday,condition_slug,condition&',
-    apiKey: 'key=65804876'
+    apiKey: 'key=ad46626c'
 }
